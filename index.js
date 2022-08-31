6e7b316188220236d656174e02d48f2ea8d4e695
weillercarvalho <engweiller@hotmail.com>
2022-08-31 10:36:37 -0300

    FEAT [index.js] (weillercarvalho) Added Joi and day.js, missing dotenv

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import Joi from "joi";
+import dayjs from "dayjs";
+import 'dayjs/locale/pt-br.js';
 
+const timestamp = Date.now()
+
+const date = dayjs().format('DD/MM/YY');
+
+console.log(date)
 
 const server = express();
 
@@ -8,20 +17,42 @@ server.use(cors());
 server.use(express.json());
 
 
+const participants = [];
+const messages = [];
+
+const userExist = (name) => {
+    return participants.filter(value => value.name === name)
+}
+
+
 server.post(`/participants`,(req,res) => {
-    server.send({message: `OK!`})
+    const { name } = req.body;
+    console.log(userExist(name))
+    participants.push({name});
+    console.log(userExist(name))
+    const schema = Joi.object({
+        username: Joi.string().min(1).max(30).required()
+    })
+    const { error } = schema.validate({username : name})
+    if (error !== undefined) {
+        return res.sendStatus(422)
+    }
+    else if(userExist(name).length > 1) {
+        return res.sendStatus(409)
+    }
+    res.sendStatus(201)
 })
 server.get(`/participants`,(req,res) => {
-    server.send({message: `OK!`})
+    res.send({message: `OK!`})
 })
 server.post(`/messages`,(req,res) => {
-    server.send({message: `OK!`})
+    res.send({message: `OK!`})
 })
 server.get(`/messages`,(req,res) => {
-    server.send({message: `OK!`})
+    res.send({message: `OK!`})
 })
 server.post(`/status`,(req,res) => {
-    server.send({message: `OK!`})
+    res.send({message: `OK!`})
 })
 
 
@@ -36,6 +67,6 @@ server.post(`/status`,(req,res) => {
 
 
 
-server.listen(5000,() => {
-    console.log(`Listening on port 5000`)
+server.listen(4000,() => {
+    console.log(`Listening on port 4000`)
 })
