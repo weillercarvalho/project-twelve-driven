7f379ffc4bcb583083ca5cc76f48a58f9e36f9e9
weillercarvalho <engweiller@hotmail.com>
2022-09-01 12:12:51 -0300

    FEAT [index.js/gitignore] (weillercarvalho) dotenv,mongodb starting.

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import Joi from "joi";
 import dayjs from "dayjs";
-import 'dayjs/locale/pt-br.js';
+import "dayjs/locale/pt-br.js";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const timestamp = Date.now()
 
 const date = dayjs().format('DD/MM/YY');
 
-console.log(date)
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect().then(() => {
+    db = mongoClient.db(`uol`)
+})
 
 const server = express();
 
@@ -17,19 +24,26 @@ server.use(cors());
 server.use(express.json());
 
 
-const participants = [];
-const messages = [];
 
-const userExist = (name) => {
-    return participants.filter(value => value.name === name)
+
+const userExist = async (name) => {
+    
+    try {
+        const finding = await db.collection(`participants`).find().toArray();
+        const newFinding = finding.filter(value => value.name === name).length;
+        // console.log(newFinding)
+        return newFinding;
+    } catch (error) {
+        return console.error(`Erro.`)
+    }
+
 }
 
 
-server.post(`/participants`,(req,res) => {
+
+server.post(`/participants`,async (req,res) => {
     const { name } = req.body;
-    console.log(userExist(name))
-    participants.push({name});
-    console.log(userExist(name))
+    console.log(await userExist(name))
     const schema = Joi.object({
         username: Joi.string().min(1).max(30).required()
     })
@@ -37,13 +51,20 @@ server.post(`/participants`,(req,res) => {
     if (error !== undefined) {
         return res.sendStatus(422)
     }
-    else if(userExist(name).length > 1) {
+    else if(await userExist(name) > 0) {
         return res.sendStatus(409)
     }
+    else if(await userExist(name) === 0) {
+        await db.collection("participants").insertOne({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: date})
+    }
+    await db.collection("participants").insertOne({name: name, lastStatus: timestamp})
     res.sendStatus(201)
 })
-server.get(`/participants`,(req,res) => {
-    res.send({message: `OK!`})
+server.get(`/participants`,async (req,res) => {
+
+    const list = await db.collection("participants").find().toArray();
+    const newList = list.map(value => ({...value,_id: undefined}))
+    res.send(newList)
 })
 server.post(`/messages`,(req,res) => {
     res.send({message: `OK!`})
